1be267fff8d2a58d20e49aa96f85c4abab0f92c1
stevenliuyi <stevenliuyi@gmail.com>
2019-08-02 20:00:46 -0400

    Updates

---
+++ precache-manifest.7b781bda53aeaecacc547b954ce3e184.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd37e62a0bc963dc383f4965ad0e9b72",
+    "url": "/depictions/index.html"
+  },
+  {
+    "revision": "78ebf52e65c025bed444",
+    "url": "/depictions/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "9e66b175f3503c53f404",
+    "url": "/depictions/static/css/main.f15b61a6.chunk.css"
+  },
+  {
+    "revision": "78ebf52e65c025bed444",
+    "url": "/depictions/static/js/2.013f3157.chunk.js"
+  },
+  {
+    "revision": "9e66b175f3503c53f404",
+    "url": "/depictions/static/js/main.f45608bf.chunk.js"
+  },
+  {
+    "revision": "b93bd9dea85a0103ca99",
+    "url": "/depictions/static/js/runtime~main.2db9d4d4.js"
+  }
+]);

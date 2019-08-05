d9d5603b8a216979fcf7213d0d12673ef32f0c6c
Travis CI User <travis@example.org>
2019-08-05 20:30:07 +0000

    Updates

--- precache-manifest.79fb38f3f0059566068f8ea55e735ace.js
+++ precache-manifest.79fb38f3f0059566068f8ea55e735ace.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6bd73f2c604c0e07cafc443891045451",
+    "revision": "aa0e1787a5fc4e6c659daae13f070c77",
     "url": "/depicts/index.html"
   },
   {
@@ -8,16 +8,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/depicts/static/css/2.266e55a5.chunk.css"
   },
   {
-    "revision": "c18b80ee91307c8ae934",
-    "url": "/depicts/static/css/main.ad263a36.chunk.css"
+    "revision": "d78663d5224a4d164c22",
+    "url": "/depicts/static/css/main.92308942.chunk.css"
   },
   {
     "revision": "1b5779831188094f184b",
     "url": "/depicts/static/js/2.78ee161b.chunk.js"
   },
   {
-    "revision": "c18b80ee91307c8ae934",
-    "url": "/depicts/static/js/main.c86648ab.chunk.js"
+    "revision": "d78663d5224a4d164c22",
+    "url": "/depicts/static/js/main.12909c86.chunk.js"
   },
   {
     "revision": "18491d1caf4adeb06a5d",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/depicts/precache-manifest.e440c505b8e133ae704332089dbefc77.js"
+  "/depicts/precache-manifest.79fb38f3f0059566068f8ea55e735ace.js"
 );
 
 self.addEventListener('message', (event) => {
